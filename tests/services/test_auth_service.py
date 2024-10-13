bfb435b68367f6f466dc7a55602437cac3012a3c
Migas77 <miguel.belchior@ua.pt>
2024-10-13 15:17:41 +0100

    SCRUM-5, SCRUM-16 | test: service layer with mocked cognito

---
+++ tests/services/test_auth_service.py
@@ -0,0 +1,88 @@
+import base64
+import os
+import pytest
+import logging
+import uuid
+from unittest.mock import patch
+import boto3
+
+from auth.user_auth import auth_with_code, user_info_with_token
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+cognito_user_client_id = "client_id"
+cognito_user_client_secret = "client_secret"
+cognito_token_endpoint = "http://token_endpoint"
+headers = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Authorization": f"Basic {base64.b64encode(f'{cognito_user_client_id}:{cognito_user_client_secret}'.encode()).decode()}"
+}
+
+
+class RequestsMockResponse:
+    def __init__(self, json_data, status_code):
+        self.json_data = json_data
+        self.status_code = status_code
+        self.text = "error reason"
+
+    def json(self):
+        return self.json_data
+
+
+@pytest.fixture(autouse=True, scope="module")
+def setup_environ():
+    logger.info("Setting up environment")
+    os.environ["COGNITO_USER_CLIENT_ID"] = cognito_user_client_id
+    os.environ["COGNITO_USER_CLIENT_SECRET"] = cognito_user_client_secret
+    os.environ["COGNITO_TOKEN_ENDPOINT"] = cognito_token_endpoint
+
+
+# 400 it's just a random error status code to test the error handling
+@patch("auth.user_auth.requests.post", return_value=RequestsMockResponse({}, 400))
+def test_unsuccessful_auth_with_code(requests_post_mock):
+    payload = {
+        "grant_type": "authorization_code",
+        "code": "code",
+        "client_id": cognito_user_client_id,
+        "redirect_uri": "redirect_uri",
+    }
+
+    result = auth_with_code("code", "redirect_uri")
+
+    requests_post_mock.assert_called_once_with(cognito_token_endpoint, data=payload, headers=headers)
+    assert result is None
+
+
+@patch("auth.user_auth.requests.post", return_value=RequestsMockResponse({"access_token": "client_access_token"}, 200))
+def test_successful_auth_with_code(requests_post_mock):
+    payload = {
+        "grant_type": "authorization_code",
+        "code": "code",
+        "client_id": cognito_user_client_id,
+        "redirect_uri": "redirect_uri",
+    }
+
+    result = auth_with_code("code", "redirect_uri")
+
+    requests_post_mock.assert_called_once_with(cognito_token_endpoint, data=payload, headers=headers)
+    assert result == "client_access_token"
+
+
+@patch("auth.user_auth.cognito_client.get_user", return_value={"ResponseMetadata": {"HTTPStatusCode": 200}})
+def test_user_info_with_token(mock_cognito_client_get_user_function):
+    result = user_info_with_token("access_token")
+
+    mock_cognito_client_get_user_function.assert_called_once_with(AccessToken="access_token")
+    assert result == {"ResponseMetadata": {"HTTPStatusCode": 200}}
+
+
+# 400 it's just a random error status code to test the error handling
+@patch("auth.user_auth.cognito_client.get_user", return_value={"ResponseMetadata": {"HTTPStatusCode": 400}})
+def test_unsuccessful_user_info_with_token(mock_cognito_client_get_user_function):
+    result = user_info_with_token("access_token_2")
+
+    mock_cognito_client_get_user_function.assert_called_once_with(AccessToken="access_token_2")
+    assert result is None
+
+
